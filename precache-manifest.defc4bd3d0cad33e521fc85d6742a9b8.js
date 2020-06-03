9520a8ce42b99482d1e613c442b74a1186387859
Symbol <Symbolistic@live.com>
2020-06-03 00:14:46 -0400

    Updates

---
+++ precache-manifest.defc4bd3d0cad33e521fc85d6742a9b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8a9ccf9faaa63117487e7fc9da0f99",
+    "url": "/usa-education-map/index.html"
+  },
+  {
+    "revision": "8cd863c080700f147ebc",
+    "url": "/usa-education-map/static/css/main.d74326f1.chunk.css"
+  },
+  {
+    "revision": "a722a24508823278863d",
+    "url": "/usa-education-map/static/js/2.3cae0e27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/usa-education-map/static/js/2.3cae0e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cd863c080700f147ebc",
+    "url": "/usa-education-map/static/js/main.d794681b.chunk.js"
+  },
+  {
+    "revision": "373140711b13d51dbe0a",
+    "url": "/usa-education-map/static/js/runtime-main.27b2c64b.js"
+  }
+]);
